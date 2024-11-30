dc3283220db02327c3e60a2736bef159300cba11
Francesco Pio Acciarino <FP.Acciarino@almaviva.it>
2024-11-30 13:02:40 +0100

    add homepage, styling, base reactiveform

--- daiet-fe/src/app/pages/home/home.component.ts
+++ daiet-fe/src/app/pages/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private formBuilder: FormBuilder) {}
 
+  chatbotForm: FormGroup = this.formBuilder.group({
+    allergeni: [''],
+    farmaci: [''],
+    patologie: [''],
+    vitalSigns: [''],
+    richiesta: [''],
+  });
+  submit() {
+    console.log(this.chatbotForm.value);
+  }
 }
