73b35c2196de23d77aad2cda54e4851bd40b37d1
Francesco Pio Acciarino <FP.Acciarino@almaviva.it>
2024-12-01 12:43:32 +0100

    edit services

--- daiet-be/src/diet-generator/diet-generator.service.ts
+++ daiet-be/src/diet-generator/diet-generator.service.ts
@@ -1,94 +1,102 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { LlmService } from 'src/llm/llm.service';
-import { PromptTemplate } from "@langchain/core/prompts";
-import { ADAPT_TO_MEDICATIONS_OUTPUT_EXAMPLE, ADAPT_TO_MEDICATIONS_PROMPT, DIET_GENERATOR_OUTPUT_EXAMPLE, DIET_GENERATOR_PROMPT } from './prompt';
+import { PromptTemplate } from '@langchain/core/prompts';
+import {
+  ADAPT_TO_MEDICATIONS_OUTPUT_EXAMPLE,
+  ADAPT_TO_MEDICATIONS_PROMPT,
+  DIET_GENERATOR_OUTPUT_EXAMPLE,
+  DIET_GENERATOR_PROMPT,
+} from './prompt';
 import { DietPlan, DietRequest, UserParameters } from './diet-generator.model';
 import { StructuredOutputParser } from '@langchain/core/dist/output_parsers';
 import { MedicationDTO } from 'src/mock-database/medications';
 
 @Injectable()
 export class DietGeneratorService {
+  constructor(private readonly llmService: LlmService) {}
 
-    constructor(
-        private readonly llmService: LlmService
-    ) { }
-
-    public async failSafeDietGeneration(data: UserParameters): Promise<any> {
-        for (let attempt = 0; attempt < 3; attempt++) {
-            try {
-                return await this._generateDiet(data);
-            } catch (error) {
-                console.error(`Error while generating diet, attempt ${attempt + 1}`, error);
-                if (attempt === 2) {
-                    return {
-                        error: 'Error while generating diet',
-                        message: error.message,
-                    };
-                }
-            }
+  public async failSafeDietGeneration(data: UserParameters): Promise<any> {
+    for (let attempt = 0; attempt < 3; attempt++) {
+      try {
+        return await this._generateDiet(data);
+      } catch (error) {
+        console.error(
+          `Error while generating diet, attempt ${attempt + 1}`,
+          error,
+        );
+        if (attempt === 2) {
+          return {
+            error: 'Error while generating diet',
+            message: error.message,
+          };
         }
+      }
     }
+  }
 
-    async _generateDiet(
-        data: UserParameters
-    ) {
+  async _generateDiet(data: UserParameters) {
+    const llm = this.llmService.llm;
 
-        const llm = this.llmService.llm;
+    const template = PromptTemplate.fromTemplate(DIET_GENERATOR_PROMPT);
+    const prompt = await template.format({
+      age: data.age,
+      weight: data.weight,
+      height: data.height,
+      goal: data.goal,
+      food_to_avoid:
+        data?.diseases?.map((d) => d.restricted_foods).join(', ') +
+        ', ' +
+        data?.allergens,
+    });
 
-        const template = PromptTemplate.fromTemplate(DIET_GENERATOR_PROMPT);
-        const prompt = await template.format({
-            age: data.age,
-            weight: data.weight,
-            height: data.height,
-            goal: data.goal,
-            food_to_avoid: data?.diseases?.map(d => d.restricted_foods).join(', ') + ', ' + data?.allergens.join(', '),
-        });
+    const response = await llm.invoke(
+      prompt + '\n' + DIET_GENERATOR_OUTPUT_EXAMPLE,
+    );
+    const plan = this._cleanMealPlan(response.content as string);
+    const fullPlan = await this._insertMedicine({
+      medications: data.medications,
+      dietPlan: plan,
+    });
+    return JSON.parse(fullPlan as string);
+  }
 
-        const response = await llm.invoke(prompt + '\n' + DIET_GENERATOR_OUTPUT_EXAMPLE);
-        const plan = this._cleanMealPlan(response.content as string);
-        const fullPlan = await this._insertMedicine({
-            medications: data.medications,
-            dietPlan: plan,
-        });
-        return JSON.parse(fullPlan as string);
+  private _cleanMealPlan(response: string): any {
+    const regex = /\{(?:[^{}]|\{(?:[^{}]|\{[^{}]*\})*\})*\}/g;
+    const matches = response.match(regex);
+    if (!matches || matches.length === 0) {
+      throw new Error('No valid JSON found in response');
     }
+    return JSON.parse(matches[0]);
+  }
 
-    private _cleanMealPlan(response: string): any {
-        const regex = /\{(?:[^{}]|\{(?:[^{}]|\{[^{}]*\})*\})*\}/g;
-        const matches = response.match(regex);
-        if (!matches || matches.length === 0) {
-            throw new Error('No valid JSON found in response');
-        }
-        return JSON.parse(matches[0]);
-    }
+  private async _insertMedicine(data: {
+    medications: MedicationDTO[];
+    dietPlan: DietPlan;
+  }) {
+    const medStr = data.medications
+      .map((medication) => this._parseMedication(medication))
+      .join('\n');
+    const planStr = JSON.stringify(data.dietPlan, null, 2);
 
-    private async _insertMedicine(
-        data: {
-            medications: MedicationDTO[];
-            dietPlan: DietPlan;
-        }
-    ) {
-        const medStr = data.medications.map(medication => this._parseMedication(medication)).join('\n');
-        const planStr = JSON.stringify(data.dietPlan, null, 2);
+    const prompt = await PromptTemplate.fromTemplate(
+      ADAPT_TO_MEDICATIONS_PROMPT,
+    ).format({
+      list_of_medications: medStr,
+      meal_plan: planStr,
+    });
 
-        const prompt = await PromptTemplate.fromTemplate(ADAPT_TO_MEDICATIONS_PROMPT).format({
-            list_of_medications: medStr,
-            meal_plan: planStr,
-        });
+    const response = await this.llmService.llm.invoke(
+      prompt + '\n' + ADAPT_TO_MEDICATIONS_OUTPUT_EXAMPLE,
+    );
+    return response.content;
+  }
 
-        const response = await this.llmService.llm.invoke(prompt + '\n' + ADAPT_TO_MEDICATIONS_OUTPUT_EXAMPLE);
-        return response.content;
-    }
-
-    private _parseMedication(
-        medication: MedicationDTO
-    ) {
-        return `
+  private _parseMedication(medication: MedicationDTO) {
+    return `
             Name: ${medication.name},
             Frequency: ${medication?.frequency},
             Additional Info: ${medication.interactions},
         `;
-    };
-
+  }
 }
--- daiet-be/src/typological/typological.controller.ts
+++ daiet-be/src/typological/typological.controller.ts
@@ -1,17 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { DISEASES_DATA } from 'src/mock-database/diseases';
-
+import { MEDICATIONS_DATA } from 'src/mock-database/medications';
 @Controller('typological')
 export class TypologicalController {
+  @Get('diseases')
+  async getDiseases() {
+    return DISEASES_DATA;
+  }
 
-    @Get('diseases')
-    async getDiseases(){
-        return DISEASES_DATA;
-    }
-
-    @Get('medications')
-    async getMedications(){
-        return DISEASES_DATA;
-    }
-    
+  @Get('medications')
+  async getMedications() {
+    return MEDICATIONS_DATA;
+  }
 }
